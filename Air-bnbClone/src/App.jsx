a9ec61c62515f001e407bcee416b94a67e87b036
KingLyrics <ekom338.com@gmail.com>
2022-10-03 14:35:46 +0100

    Card data required

--- Air-bnbClone/src/App.jsx
+++ Air-bnbClone/src/App.jsx
@@ -1,6 +1,5 @@
-
+import CardData from "./CardData"
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
---
+++ Air-bnbClone/src/CardData.js
@@ -0,0 +1,61 @@
+export default [
+    {
+        id:"01",
+        imageUrl: "",
+        location: "Salobrena, Spain",
+        rating: "4.8",
+        distance: "3,443 Kilometers",
+        duration: "Jan 3-8",
+        price: "$508 night",
+    },
+
+    {
+        id:"02",
+        imageUrl: "",
+        location: "El,Port de la Selva Spain",
+        rating: "4.8",
+        distance: "3,991 Kilometers",
+        duration: "Oct 10-15",
+        price: "$450 night",
+    },
+
+    {
+        id:"03",
+        imageUrl: "",
+        location: "Ponta Delgada, Portugal",
+        rating: "4.8",
+        distance: "4,564 Kilometers",
+        duration: "Apr 15-21",
+        price: "$196 night",
+    },
+
+    {
+        id:"04",
+        imageUrl: "",
+        location: "Jochnery, France",
+        rating: "4.8",
+        distance: "4,582 Kilometers",
+        duration: "Mar 31 - Apr 6",
+        price: "$508 night",
+    },
+
+    {
+        id:"05",
+        imageUrl: "",
+        location: "Guyonvelle, France",
+        rating: "4.8",
+        distance: "3,443 Kilometers",
+        duration: "Dec 12-17",
+        price: "$107 night",
+    },
+
+    {
+        id:"06",
+        imageUrl: "",
+        location: "Caylus, France",
+        rating: "4.8",
+        distance: "3,443 Kilometers",
+        duration: "Jan 3-8",
+        price: "$508 night",
+    }
+]
